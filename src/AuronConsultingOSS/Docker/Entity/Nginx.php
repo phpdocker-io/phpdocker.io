5312dc7d8a3a6ca3d8151221c5ac9dc66dbee9c3
Luis Pabon <luis.pabon@rightster.com>
2016-01-07 22:13:30 +0000

    Initial commit

---
+++ src/AuronConsultingOSS/Docker/Entity/Nginx.php
@@ -0,0 +1,7 @@
+<?php
+namespace AuronConsultingOSS\Docker\Entity;
+
+class Nginx
+{
+
+}
---
+++ src/AuronConsultingOSS/Docker/Entity/Project.php
@@ -0,0 +1,15 @@
+<?php
+namespace AuronConsultingOSS\Docker\Entity;
+
+class Project
+{
+    protected $name;
+
+    protected $nginxOptions;
+
+    protected $mysqlOptions;
+
+    protected $phpOptions;
+
+    protected $memcachedOptions;
+}
