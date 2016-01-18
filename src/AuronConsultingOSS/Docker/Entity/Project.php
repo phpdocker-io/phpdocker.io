c487a6b98ac1d0bb5e891a105fbfb1d09b2fe733
Luis Pabon <luis.pabon@rightster.com>
2016-01-18 21:04:33 +0000

    Refactor project, add new entities, redirect has** methods to entity isEnabled

--- src/AuronConsultingOSS/Docker/Entity/Project.php
+++ src/AuronConsultingOSS/Docker/Entity/Project.php
@@ -1,6 +1,10 @@
 <?php
 namespace AuronConsultingOSS\Docker\Entity;
 
+use AuronConsultingOSS\Docker\Exception\MissingDependencyException;
+use AuronConsultingOSS\Docker\Interfaces\HostnameSuffixInterface;
+use Cocur\Slugify\Slugify;
+
 /**
  * Defines a single project.
  *
@@ -35,25 +39,75 @@ class Project
     protected $phpOptions;
 
     /**
-     * @var bool
+     * @var MemcachedOptions
      */
-    protected $hasMemcached = false;
+    protected $memcachedOptions;
 
     /**
-     * @var bool
+     * @var RedisOptions
      */
-    protected $hasRedis = false;
+    protected $redisOptions;
 
     /**
-     * @var bool
+     * @var MailcatcherOptions
      */
-    protected $hasMailcatcher = false;
+    protected $mailCatcherOptions;
+
+    /**
+     * @var string
+     */
+    protected $projectNameSlug;
+
+    /**
+     * @var Slugify
+     */
+    private $slugifier;
+
+    /**
+     * @var array
+     */
+    private $hostnamesForServices = [];
 
     public function __construct()
     {
-        $this->nginxOptions = new NginxOptions();
-        $this->mysqlOptions = new MySQLOptions();
-        $this->phpOptions   = new PhpOptions();
+        $this->nginxOptions       = new NginxOptions();
+        $this->mysqlOptions       = new MySQLOptions();
+        $this->phpOptions         = new PhpOptions();
+        $this->redisOptions       = new RedisOptions();
+        $this->memcachedOptions   = new MemcachedOptions();
+        $this->mailCatcherOptions = new MailcatcherOptions();
+    }
+
+    /**
+     * Calculates the hostname of a service based on the project name, and the service's hostname suffix.
+     *
+     * @param HostnameSuffixInterface $service
+     *
+     * @return string
+     */
+    public function getHostnameForService(HostnameSuffixInterface $service) : string
+    {
+        $serviceKey = get_class($service);
+        if (array_key_exists($serviceKey, $this->hostnamesForServices) === false) {
+            $this->hostnamesForServices[$serviceKey] = sprintf('%s-%s', $this->getProjectNameSlug(), $service->getHostnameSuffix());
+        }
+
+        return $this->hostnamesForServices[$serviceKey];
+    }
+
+    /**
+     * Returns a slugged-up version of the project name.
+     *
+     * @return string
+     * @throws MissingDependencyException
+     */
+    public function getProjectNameSlug() : string
+    {
+        if ($this->projectNameSlug === null) {
+            $this->projectNameSlug = $this->getSlugifier()->slugify($this->getName());
+        }
+
+        return $this->projectNameSlug;
     }
 
     /**
@@ -89,7 +143,7 @@ class Project
      *
      * @return Project
      */
-    public function setBasePort($basePort)
+    public function setBasePort($basePort) : self
     {
         $this->basePort = $basePort;
 
@@ -101,7 +155,7 @@ class Project
      */
     public function hasNginx() : bool
     {
-        return $this->nginxOptions === null;
+        return $this->nginxOptions->isEnabled();
     }
 
     /**
@@ -129,7 +183,7 @@ class Project
      */
     public function hasMysql() : bool
     {
-        return $this->mysqlOptions === null;
+        return $this->mysqlOptions->isEnabled();
     }
 
     /**
@@ -173,11 +227,31 @@ class Project
     }
 
     /**
+     * @return MemcachedOptions
+     */
+    public function getMemcachedOptions() : MemcachedOptions
+    {
+        return $this->memcachedOptions;
+    }
+
+    /**
+     * @param MemcachedOptions $memcachedOptions
+     *
+     * @return Project
+     */
+    public function setMemcachedOptions(MemcachedOptions $memcachedOptions) : self
+    {
+        $this->memcachedOptions = $memcachedOptions;
+
+        return $this;
+    }
+
+    /**
      * @return bool
      */
     public function hasMemcached() : bool
     {
-        return $this->hasMemcached;
+        return $this->memcachedOptions->isEnabled();
     }
 
     /**
@@ -187,7 +261,27 @@ class Project
      */
     public function setHasMemcached(bool $hasMemcached) : self
     {
-        $this->hasMemcached = $hasMemcached;
+        $this->memcachedOptions->setEnabled($hasMemcached);
+
+        return $this;
+    }
+
+    /**
+     * @return RedisOptions
+     */
+    public function getRedisOptions() : RedisOptions
+    {
+        return $this->redisOptions;
+    }
+
+    /**
+     * @param RedisOptions $redisOptions
+     *
+     * @return Project
+     */
+    public function setRedisOptions(RedisOptions $redisOptions) : self
+    {
+        $this->redisOptions = $redisOptions;
 
         return $this;
     }
@@ -197,7 +291,7 @@ class Project
      */
     public function hasRedis() : bool
     {
-        return $this->hasRedis;
+        return $this->redisOptions->isEnabled();
     }
 
     /**
@@ -207,7 +301,27 @@ class Project
      */
     public function setHasRedis(bool $hasRedis) : self
     {
-        $this->hasRedis = $hasRedis;
+        $this->redisOptions->setEnabled($hasRedis);
+
+        return $this;
+    }
+
+    /**
+     * @return MailcatcherOptions
+     */
+    public function getMailCatcherOptions() : MailcatcherOptions
+    {
+        return $this->mailCatcherOptions;
+    }
+
+    /**
+     * @param MailcatcherOptions $mailCatcherOptions
+     *
+     * @return Project
+     */
+    public function setMailCatcherOptions(MailcatcherOptions $mailCatcherOptions) : self
+    {
+        $this->mailCatcherOptions = $mailCatcherOptions;
 
         return $this;
     }
@@ -215,9 +329,9 @@ class Project
     /**
      * @return boolean
      */
-    public function hasMailcatcher()
+    public function hasMailcatcher() : bool
     {
-        return $this->hasMailcatcher;
+        return $this->mailCatcherOptions->isEnabled();
     }
 
     /**
@@ -227,7 +341,32 @@ class Project
      */
     public function setHasMailcatcher(bool $hasMailcatcher) : self
     {
-        $this->hasMailcatcher = $hasMailcatcher;
+        $this->mailCatcherOptions->setEnabled($hasMailcatcher);
+
+        return $this;
+    }
+
+    /**
+     * @return Slugify
+     * @throws MissingDependencyException
+     */
+    public function getSlugifier() : Slugify
+    {
+        if ($this->slugifier === null) {
+            throw new MissingDependencyException('Slugifier hasn\'t been initialised');
+        }
+
+        return $this->slugifier;
+    }
+
+    /**
+     * @param Slugify $slugifier
+     *
+     * @return Project
+     */
+    public function setSlugifier(Slugify $slugifier) : self
+    {
+        $this->slugifier = $slugifier;
 
         return $this;
     }
