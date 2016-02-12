a59991e473284ecea131a46f2ea1a7ecb7c5afc8
Luis Pabon <luis.pabon@rightster.com>
2016-02-12 15:34:51 +0000

    Add bootstrap multiselect

--- src/AppBundle/Resources/public/js/main-form.js
+++ src/AppBundle/Resources/public/js/main-form.js
@@ -11,25 +11,33 @@ function doMainFormMagic() {
     var mysqlSwitch        = $('#project_mysqlOptions_hasMysql');
 
     // Disable mysql options
-    var disableMysqlOptions = function() {
+    var disableMysqlOptions = function () {
         mysqlOptionsDiv.addClass('disabled').fadeIn();
         mysqlOptionsFields.prop('disabled', true);
     };
 
     disableMysqlOptions();
 
-    var enableMysqlOptions = function() {
+    var enableMysqlOptions = function () {
         mysqlOptionsDiv.removeClass('disabled');
         mysqlOptionsFields.prop('disabled', false);
     };
 
     // Enable mysql options when clicking the switch
-    mysqlSwitch.change(function() {
+    mysqlSwitch.change(function () {
         if (mysqlSwitch.prop('checked') == true) {
             enableMysqlOptions();
         } else {
             disableMysqlOptions();
         }
     });
+
+    $('#project_phpOptions_phpExtensions').multiselect({
+        enableCaseInsensitiveFiltering: true,
+        maxHeight: 250,
+        buttonWidth: 300,
+        dropUp: true
+    });
+
 };
 
