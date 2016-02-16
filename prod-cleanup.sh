fc50a6fe952c5768c905bdf9b3076043c2534bfe
Luis Pabon <luis.pabon@rightster.com>
2016-02-16 14:21:37 +0000

    Bower install on prod deployment

--- prod-cleanup.sh
+++ prod-cleanup.sh
@@ -4,6 +4,7 @@ sudo rm /tmp/phpdocker.io/* -Rf
 composer -o install
 sudo php bin/console cache:clear --env=prod
 sudo rm -Rf web/bundles web/css web/js
+bower install
 sudo php bin/console assets:install --env=prod
 sudo php bin/console assetic:dump --env=prod
 sudo chown www-data:www-data /tmp/phpdocker.io/* -Rf
