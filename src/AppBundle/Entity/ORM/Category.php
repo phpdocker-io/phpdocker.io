ed263f8fa1a6efb6c11d360566345f393c4d250c
Luis Pabon <luis.pabon@rightster.com>
2016-03-24 01:48:51 +0000

    Fix categories imports and add Category into EasyAdmin

---
+++ src/AppBundle/Entity/ORM/Category.php
@@ -0,0 +1,59 @@
+<?php
+namespace AppBundle\Entity\ORM;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Categories
+ *
+ * @ORM\Entity()
+ *
+ * @package   AppBundle\Entity\ORM
+ * @copyright Auron Consulting Ltd
+ */
+class Category
+{
+    /**
+     * Bring in primary key
+     */
+    use PropertyTrait\PrimaryKeyTrait;
+
+    /**
+     * Bring in title property
+     */
+    use PropertyTrait\TitleTrait;
+
+    /**
+     * Bring in timestamps and their doctrine behaviors
+     */
+    use PropertyTrait\TimestampableTrait;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Gedmo\Slug(fields={"title"})
+     */
+    private $slug;
+
+    /**
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param string $slug
+     *
+     * @return self
+     */
+    public function setSlug(string $slug) : self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
