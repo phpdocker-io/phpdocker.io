8f4ef559f060e5248719d7686ac9afcf5b902f2c
Luis Pabon <luis.pabon@rightster.com>
2016-03-24 02:47:26 +0000

    Reorganise prod script better, fix var permissions even moar

--- prepare-prod.sh
+++ prepare-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# First, install composer and bower dependencies
-composer -o install
-rm src/AppBundle/Resources/public/vendor/* -Rf
-bower install
-
 # Cleanup
-sudo php bin/console cache:clear --env=prod
 sudo rm -Rf web/bundles web/css web/js
+sudo rm -Rf var/* -Rf
+rm src/AppBundle/Resources/public/vendor/* -Rf
+
+# Install composer and bower dependencies
+composer -o install
+bower install
 
 # Install assets
 sudo php bin/console assets:install --env=prod
@@ -15,4 +15,4 @@ sudo php bin/console assetic:dump --env=prod
 
 # Prime cache & unbork cache and log permissions
 sudo php bin/console cache:warmup --env=prod
-sudo chown www-data:www-data ../var/* -Rf
+sudo chown www-data:www-data var/* -Rf
