966944e8f36beed02eed2262daecbdc48a0ff228
Luis Pabon <luis.pabon@rightster.com>
2016-03-25 19:04:00 +0000

    Try fix dodgy migrations

--- app/DoctrineMigrations/Version20160324131749.php
+++ app/DoctrineMigrations/Version20160324131749.php
@@ -25,8 +25,7 @@ class Version20160324131749 extends AbstractMigration implements ContainerAwareI
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE post ADD category_id INT NOT NULL');
-        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE post ADD category_id INT');
         $this->addSql('CREATE INDEX IDX_5A8A6C8D12469DE2 ON post (category_id)');
     }
 
@@ -65,7 +64,6 @@ class Version20160324131749 extends AbstractMigration implements ContainerAwareI
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D12469DE2');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP INDEX IDX_5A8A6C8D12469DE2 ON post');
         $this->addSql('ALTER TABLE post DROP category_id');
--- app/DoctrineMigrations/Version20160324151907.php
+++ app/DoctrineMigrations/Version20160324151907.php
@@ -18,7 +18,7 @@ class Version20160324151907 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE post DROP INDEX UNIQ_5A8A6C8D12469DE2, ADD INDEX IDX_5A8A6C8D12469DE2 (category_id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE post CHANGE category_id category_id INT NOT NULL');
     }
 
@@ -30,7 +30,7 @@ class Version20160324151907 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE post DROP INDEX IDX_5A8A6C8D12469DE2, ADD UNIQUE INDEX UNIQ_5A8A6C8D12469DE2 (category_id)');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D12469DE2');
         $this->addSql('ALTER TABLE post CHANGE category_id category_id INT DEFAULT NULL');
     }
 }
