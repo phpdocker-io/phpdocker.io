17caa11d7c4795785e666a1c329b3a780633c2be
Luis Pabon <luis.pabon@rightster.com>
2016-03-29 12:23:46 +0100

    ContactRequest really shouldn't extend AbstractGeneratorType

--- src/AppBundle/Form/PostCommentType.php
+++ src/AppBundle/Form/PostCommentType.php
@@ -17,6 +17,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\ORM\PostComment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -58,8 +59,6 @@ class PostCommentType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\ORM\PostComment'
-        ));
+        $resolver->setDefaults(['data_class' => PostComment::class]);
     }
 }
