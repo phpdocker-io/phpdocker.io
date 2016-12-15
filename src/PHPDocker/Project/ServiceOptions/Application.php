fe000e1a5744061b3c7fee782460b4c56123ad6a
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2016-12-15 11:38:27 +0000

    Add support for Silex application type

--- src/PHPDocker/Project/ServiceOptions/Application.php
+++ src/PHPDocker/Project/ServiceOptions/Application.php
@@ -31,6 +31,7 @@ class Application
     const APPLICATION_TYPE_SYMFONY = 'symfony';
     const APPLICATION_TYPE_PHALCON = 'phalcon';
     const APPLICATION_TYPE_GENERIC = 'generic';
+    const APPLICATION_TYPE_SILEX   = 'silex';
 
     /**
      * Allowed application types with short description
@@ -39,6 +40,7 @@ class Application
         self::APPLICATION_TYPE_GENERIC => 'Generic: Zend, Laravel, Lumen...',
         self::APPLICATION_TYPE_SYMFONY => 'Symfony 2/3',
         self::APPLICATION_TYPE_PHALCON => 'Phalcon 3 (PHP 5.6/7.0 only)',
+        self::APPLICATION_TYPE_SILEX   => 'Silex',
     ];
 
     /**
