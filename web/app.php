3f55850275a547c8e9dc5e2bea574ee01eefa59e
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2017-07-23 22:55:22 +0100

    Simplify front controller further

--- web/app.php
+++ web/app.php
@@ -1,21 +1,10 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
-$loader = require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Capture app environment & enable debug on dev
-$env = strtolower(getenv('SYMFONY_ENV') ?: 'prod');
-
-if ($env === 'dev') {
-    Debug::enable();
-}
-
-// Kernel & dispatch
+$env      = strtolower(getenv('SYMFONY_ENV') ?: 'prod');
 $kernel   = new AppKernel($env, $env !== 'prod');
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request)->send();
