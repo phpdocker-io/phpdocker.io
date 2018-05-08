c859e8f193228b0cd4dcdc10559769e7ed6f6c28
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2018-05-08 11:58:48 +0100

    Add logging and mailing

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
