67336045228e2f219fc72111aec2dc9c93182384
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-02-15 16:19:22 +0000

    Move posts list to separate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,16 @@
 import React, { Component } from 'react'
+import List from './Posts/List'
 import './App.css'
 import 'semantic-ui-css/semantic.min.css'
 
-const postApiBaseUrl = require('./config').postApiBaseUrl
-
 class App extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      posts: []
-    }
-  }
-
-  componentDidMount () {
-    let posts = []
-
-    const request = new Request(postApiBaseUrl, {
-      method: 'GET',
-      headers: new Headers()
-    })
-
-    request.headers.append('accept', 'application/json')
-
-    fetch(request)
-      .then(response => {
-        return response.json()
-      })
-      .then(json => {
-        posts = json.map((post) => {
-          return {
-            title: post.title,
-            bodyIntro: post.bodyIntro,
-            slug: post.slug,
-
-          }
-        })
-
-        this.setState({
-          posts: posts
-        })
-      })
-
-  }
-
-  news () {
-    let posts = []
-
-    this.state.posts.map(post => {
-      return posts.push(
-        <div className="post" key={post.slug}>
-          <h2>{post.title}</h2>
-          <div>{post.bodyIntro}</div>
-        </div>
-      )
-    })
-
-    return posts
-  }
 
   render () {
 
     return (
       <div>
-        <h1>News</h1>
-        {this.news()}
+        <List/>
       </div>
-
     )
   }
 }
---
+++ frontend/src/Posts/List.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+const postApiBaseUrl = require('../config').postApiBaseUrl
+
+
+
+class List extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount () {
+    let posts = []
+
+    const request = new Request(postApiBaseUrl, {
+      method: 'GET',
+      headers: new Headers()
+    })
+
+    request.headers.append('accept', 'application/json')
+
+    fetch(request)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        posts = json.map((post) => {
+          return {
+            title: post.title,
+            bodyIntro: post.bodyIntro,
+            slug: post.slug,
+
+          }
+        })
+
+        this.setState({
+          posts: posts
+        })
+      })
+
+  }
+
+  news () {
+    let posts = []
+
+    this.state.posts.map(post => {
+      return posts.push(
+        <div className="post" key={post.slug}>
+          <h2>{post.title}</h2>
+          <div>{post.bodyIntro}</div>
+        </div>
+      )
+    })
+
+    return posts
+  }
+
+  render () {
+
+    return (
+      <div>
+        <h1>News</h1>
+        {this.news()}
+      </div>
+
+    )
+  }
+}
+
+export default List
