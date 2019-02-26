17809a6e9024ea0ec3ef626b1b2b9b36f1adb805
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-02-26 17:06:26 +0000

    Start adding basic page structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import List from './Posts/List'
+import Generator from './Generator/Generator'
+import Contact from './Contact'
 import './App.css'
 import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
 
   render () {
-
     return (
       <div>
-        <List/>
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/generator">Generator</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={List} />
+            <Route path="/generator" component={Generator} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
       </div>
     )
   }
---
+++ frontend/src/Contact.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+const { postApiBaseUrl } = require('./config')
+
+class List extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+    }
+  }
+
+  componentDidMount () {
+
+  }
+
+
+  render () {
+
+    return (
+      <div>
+        <h1>Contact</h1>
+      </div>
+    )
+  }
+}
+
+export default List
