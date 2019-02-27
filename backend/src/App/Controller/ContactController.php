c4a7456a36cbc99f02ad113dfbb62c3020b8cf26
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-02-27 11:31:36 +0000

    Add copyright notice to new files

--- backend/src/App/Controller/ContactController.php
+++ backend/src/App/Controller/ContactController.php
@@ -1,25 +1,59 @@
 <?php
+/**
+ * Copyright 2019 Luis Alberto Pabón Flores
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 namespace App\Controller;
 
-use PHPDocker\Contact\DispatcherInterface;
+use PHPDocker\Contact\DispatcherInterface as EmailDispatcher;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface as Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface as Validator;
 
 class ContactController
 {
     /**
-     * @var DispatcherInterface
+     * @var EmailDispatcher
      */
     private $emailDispatcher;
 
-    public function __construct(DispatcherInterface $emailDispatcher)
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    /**
+     * @var Validator
+     */
+    private $validator;
+
+    public function __construct(EmailDispatcher $emailDispatcher, Serializer $serializer, Validator $validator)
     {
         $this->emailDispatcher = $emailDispatcher;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
     }
 
     public function process(Request $request): JsonResponse
     {
+        $message = $this->serializer->deserialize($request->getContent(), 'json');
+
+
         return new JsonResponse(['ok']);
     }
 }
