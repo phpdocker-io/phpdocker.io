2fa37ef00d8f36ee2589a37c5c5b39211bc9c97e
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-03-02 01:50:26 +0000

    Re-add semantic ui as it does indeed play nice with formik. As a POF, use semantic's menu with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,33 +3,53 @@ import List from './Posts/List'
 import Generator from './Generator/Generator'
 import Contact from './Contact'
 import './App.css'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
+import { Menu } from 'semantic-ui-react'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 class App extends Component {
+  state = {}
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render () {
+    const { activeItem } = this.state
+
     return (
       <div>
 
+
         <Router>
           <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/generator">Generator</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
+            <Menu>
+              <Menu.Item
+                as={Link} to="/"
+                name='home'
+                active={activeItem === 'home'}
+                content='Home'
+                onClick={this.handleItemClick}
+              />
 
-            <hr />
+              <Menu.Item
+                as={Link} to="/generator"
+                name='generator'
+                active={activeItem === 'generator'}
+                content='Generator'
+                onClick={this.handleItemClick}
 
-            <Route exact path="/" component={List} />
-            <Route path="/generator" component={Generator} />
-            <Route path="/contact" component={Contact} />
+              />
+
+              <Menu.Item
+                as={Link} to="/contact"
+                name='contact'
+                active={activeItem === 'Contact'}
+                content='Contact'
+                onClick={this.handleItemClick}
+              />
+            </Menu>
+            
+            <Route exact path="/" component={List}/>
+            <Route path="/generator" component={Generator}/>
+            <Route path="/contact" component={Contact}/>
           </div>
         </Router>
       </div>
