9b19fa43668ca507fd8db875615aa768e4e51045
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-06-15 13:24:34 +0100

    Add both symfony debug & monolog

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
