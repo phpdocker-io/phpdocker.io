c2fa2ae9af2a9c73d0fa6b3474031b17cf9a59b6
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-06-15 10:04:15 +0100

    Add twig bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
