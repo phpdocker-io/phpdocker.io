9508b69e7049753027026b32ca25ba8fa0cfbba3
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-07-26 11:49:33 +0100

    Share bootstrap between behat & phpunit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * Test environment (behat & phpunit).
+ *
+ * Loads .env support.
+ */
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
