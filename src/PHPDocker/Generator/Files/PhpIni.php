0f023ea77d0e9a2aeb74dde41a4096a1cad14034
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2024-08-16 11:07:51 +0100

    Remove unused import

--- src/PHPDocker/Generator/Files/PhpIni.php
+++ src/PHPDocker/Generator/Files/PhpIni.php
@@ -20,7 +20,6 @@ declare(strict_types=1);
 namespace App\PHPDocker\Generator\Files;
 
 use App\PHPDocker\Interfaces\GeneratedFileInterface;
-use App\PHPDocker\Project\Project;
 use Twig\Environment;
 
 readonly class PhpIni implements GeneratedFileInterface
