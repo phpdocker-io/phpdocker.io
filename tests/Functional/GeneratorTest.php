e3a326bc90dd0cc7dfe641907a70509fc14e78ff
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2026-05-27 11:55:43 +0100

    step-1: Migrate Behat scenarios to PHPUnit functional tests

--- tests/Functional/GeneratorTest.php
+++ tests/Functional/GeneratorTest.php
@@ -197,4 +197,310 @@ class GeneratorTest extends WebTestCase
 
         return $fileContent;
     }
+
+    #[Test]
+    public function generatorRequiresABasePort(): void
+    {
+        $this->client->request('GET', '/');
+        $this->client->submitForm('Generate project archive', [
+            'project[globalOptions][basePort]' => '',
+        ]);
+
+        self::assertSelectorTextContains('#container_for_basePort', 'This value should not be blank.');
+    }
+
+    #[Test]
+    public function testDefaultZipGeneration(): void
+    {
+        $this->client->request('GET', '/');
+        $this->client->submitForm('Generate project archive');
+
+        self::assertResponseIsSuccessful();
+
+        $response = $this->client->getResponse();
+        self::assertInstanceOf(BinaryFileResponse::class, $response);
+        self::assertStringContainsString('phpdocker.zip', $response->headers->get('Content-Disposition') ?? '');
+    }
+
+    #[Test]
+    public function testDefaultZipContainsAllExpectedFiles(): void
+    {
+        $this->generateAndGetZip(['project[globalOptions][basePort]' => '8000']);
+
+        self::assertTrue($this->zipContainsFile('docker-compose.yml'));
+        self::assertTrue($this->zipContainsFile('phpdocker/php-fpm/Dockerfile'));
+        self::assertTrue($this->zipContainsFile('phpdocker/php-fpm/php-ini-overrides.ini'));
+        self::assertTrue($this->zipContainsFile('phpdocker/nginx/nginx.conf'));
+        self::assertTrue($this->zipContainsFile('phpdocker/README.md'));
+        self::assertTrue($this->zipContainsFile('phpdocker/README.html'));
+    }
+
+    #[Test]
+    public function testWebserverAndPhpFpmAlwaysPresent(): void
+    {
+        $this->generateAndGetZip(['project[globalOptions][basePort]' => '8000']);
+
+        $this->zipFileContains('docker-compose.yml', 'webserver:');
+        $this->zipFileContains('docker-compose.yml', 'php-fpm:');
+    }
+
+    #[Test]
+    public function testPhp82ReflectedInDockerfile(): void
+    {
+        $this->generateAndGetZip([
+            'project[phpOptions][version]'     => '8.2',
+            'project[globalOptions][basePort]' => '8000',
+        ]);
+
+        $this->zipFileContains('phpdocker/php-fpm/Dockerfile', 'phpdockerio/php:8.2-fpm');
+    }
+
+    #[Test]
+    public function testPhp85ReflectedInDockerfile(): void
+    {
+        $this->generateAndGetZip([
+            'project[phpOptions][version]'     => '8.5',
+            'project[globalOptions][basePort]' => '8000',
+        ]);
+
+        $this->zipFileContains('phpdocker/php-fpm/Dockerfile', 'phpdockerio/php:8.5-fpm');
+    }
+
+    #[Test]
+    public function testMysqlValidationWorks(): void
+    {
+        $this->client->request('GET', '/');
+        $this->client->submitForm('Generate project archive', [
+            'project[mysqlOptions][hasMysql]' => '1',
+        ]);
+
+        self::assertSelectorTextContains('#container_for_mysql_rootPassword', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mysql_databaseName', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mysql_username', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mysql_password', 'This value should not be blank.');
+    }
+
+    #[Test]
+    public function testMariadbValidationWorks(): void
+    {
+        $this->client->request('GET', '/');
+        $this->client->submitForm('Generate project archive', [
+            'project[mariadbOptions][hasMariadb]' => '1',
+        ]);
+
+        self::assertSelectorTextContains('#container_for_mariadb_rootPassword', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mariadb_databaseName', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mariadb_username', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_mariadb_password', 'This value should not be blank.');
+    }
+
+    #[Test]
+    public function testPostgresValidationWorks(): void
+    {
+        $this->client->request('GET', '/');
+        $this->client->submitForm('Generate project archive', [
+            'project[postgresOptions][hasPostgres]' => '1',
+        ]);
+
+        self::assertSelectorTextContains('#container_for_postgres_rootUser', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_postgres_rootPassword', 'This value should not be blank.');
+        self::assertSelectorTextContains('#container_for_postgres_databaseName', 'This value should not be blank.');
+    }
+
+    #[Test]
+    public function testMysqlConfigWorksCorrectly(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]'    => '8000',
+            'project[phpOptions][version]'        => '8.4',
+            'project[mysqlOptions][hasMysql]'     => '1',
+            'project[mysqlOptions][rootPassword]' => 'root pass',
+            'project[mysqlOptions][databaseName]' => 'db name',
+            'project[mysqlOptions][username]'     => 'user',
+            'project[mysqlOptions][password]'     => 'pass',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'mysql:');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_ROOT_PASSWORD=root pass');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_DATABASE=db name');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_USER=user');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_PASSWORD=pass');
+    }
+
+    #[Test]
+    public function testMariadbConfigWorksCorrectly(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]'       => '8000',
+            'project[phpOptions][version]'           => '8.4',
+            'project[mariadbOptions][hasMariadb]'     => '1',
+            'project[mariadbOptions][rootPassword]'  => 'root pass',
+            'project[mariadbOptions][databaseName]'  => 'db name',
+            'project[mariadbOptions][username]'      => 'user',
+            'project[mariadbOptions][password]'      => 'pass',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'mariadb:');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_ROOT_PASSWORD=root pass');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_DATABASE=db name');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_USER=user');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_PASSWORD=pass');
+    }
+
+    #[Test]
+    public function testPostgresConfigWorksCorrectly(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]'         => '8000',
+            'project[phpOptions][version]'             => '8.4',
+            'project[postgresOptions][hasPostgres]'    => '1',
+            'project[postgresOptions][rootUser]'       => 'root user',
+            'project[postgresOptions][rootPassword]'   => 'root pass',
+            'project[postgresOptions][databaseName]'   => 'db name',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'postgres:');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_USER=root user');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_PASSWORD=root pass');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_DB=db name');
+    }
+
+    #[Test]
+    public function testRedisIncludedWhenEnabled(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]' => '8000',
+            'project[phpOptions][version]'     => '8.4',
+            'project[hasRedis]'                => '1',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'redis:');
+    }
+
+    #[Test]
+    public function testMemcachedIncludedWhenEnabled(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]' => '8000',
+            'project[phpOptions][version]'     => '8.4',
+            'project[hasMemcached]'            => '1',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'memcached:');
+    }
+
+    #[Test]
+    public function testMailhogIncludedWhenEnabled(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]' => '8000',
+            'project[phpOptions][version]'     => '8.4',
+            'project[hasMailhog]'              => '1',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'mailhog:');
+    }
+
+    #[Test]
+    public function testClickhouseIncludedWhenEnabled(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]' => '8000',
+            'project[phpOptions][version]'     => '8.4',
+            'project[hasClickhouse]'           => '1',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'clickhouse:');
+    }
+
+    #[Test]
+    public function testOptionalServicesAbsentByDefault(): void
+    {
+        $this->generateAndGetZip(['project[globalOptions][basePort]' => '8000']);
+
+        $this->zipFileNotContains('docker-compose.yml', 'redis:');
+        $this->zipFileNotContains('docker-compose.yml', 'memcached:');
+        $this->zipFileNotContains('docker-compose.yml', 'mailhog:');
+        $this->zipFileNotContains('docker-compose.yml', 'clickhouse:');
+        $this->zipFileNotContains('docker-compose.yml', 'mysql:');
+        $this->zipFileNotContains('docker-compose.yml', 'mariadb:');
+        $this->zipFileNotContains('docker-compose.yml', 'postgres:');
+        $this->zipFileNotContains('docker-compose.yml', 'elasticsearch:');
+    }
+
+    #[Test]
+    public function testAllOptionalServicesEnabledSimultaneously(): void
+    {
+        $this->generateAndGetZip([
+            'project[globalOptions][basePort]'                => '8000',
+            'project[phpOptions][version]'                    => '8.4',
+            'project[hasRedis]'                               => '1',
+            'project[hasMemcached]'                           => '1',
+            'project[hasMailhog]'                             => '1',
+            'project[hasClickhouse]'                          => '1',
+            'project[mysqlOptions][hasMysql]'                 => '1',
+            'project[mysqlOptions][rootPassword]'             => 'root pass',
+            'project[mysqlOptions][databaseName]'             => 'db name',
+            'project[mysqlOptions][username]'                 => 'user',
+            'project[mysqlOptions][password]'                 => 'pass',
+            'project[mariadbOptions][hasMariadb]'             => '1',
+            'project[mariadbOptions][rootPassword]'          => 'root pass',
+            'project[mariadbOptions][databaseName]'          => 'db name',
+            'project[mariadbOptions][username]'              => 'user',
+            'project[mariadbOptions][password]'              => 'pass',
+            'project[postgresOptions][hasPostgres]'           => '1',
+            'project[postgresOptions][rootUser]'              => 'root user',
+            'project[postgresOptions][rootPassword]'          => 'root pass',
+            'project[postgresOptions][databaseName]'          => 'db name',
+        ]);
+
+        $this->zipFileContains('docker-compose.yml', 'redis:');
+        $this->zipFileContains('docker-compose.yml', 'memcached:');
+        $this->zipFileContains('docker-compose.yml', 'mailhog:');
+        $this->zipFileContains('docker-compose.yml', 'clickhouse:');
+        $this->zipFileContains('docker-compose.yml', 'mysql:');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_ROOT_PASSWORD=root pass');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_DATABASE=db name');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_USER=user');
+        $this->zipFileContains('docker-compose.yml', 'MYSQL_PASSWORD=pass');
+        $this->zipFileContains('docker-compose.yml', 'mariadb:');
+        $this->zipFileContains('docker-compose.yml', 'postgres:');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_USER=root user');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_PASSWORD=root pass');
+        $this->zipFileContains('docker-compose.yml', 'POSTGRES_DB=db name');
+    }
+
+    private function zipContainsFile(string $filename): bool
+    {
+        $response = $this->client->getResponse();
+        if (!$response instanceof BinaryFileResponse) {
+            return false;
+        }
+
+        $path = $response->getFile()->getPathname();
+        if (!is_file($path)) {
+            return false;
+        }
+
+        $zip    = new ZipArchive();
+        $result = $zip->open($path);
+        if ($result !== true) {
+            return false;
+        }
+
+        $content = $zip->getFromName($filename);
+        $zip->close();
+
+        return $content !== false;
+    }
+
+    private function zipFileContains(string $filename, string $expected): void
+    {
+        self::assertStringContainsString($expected, $this->getZipFileContent($filename));
+    }
+
+    private function zipFileNotContains(string $filename, string $expected): void
+    {
+        self::assertStringNotContainsString($expected, $this->getZipFileContent($filename));
+    }
 }
